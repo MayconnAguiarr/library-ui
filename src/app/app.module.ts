2a0adfa1629287459e0c37333bd27917fb7412ba
Maycon <mayconn.aguiarr@gmail.com>
2018-02-25 12:22:59 -0300

    adicionado novo Style em 'angular-cli.json', novos importes em app.module.ts  e anteração no html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import{DataTableModule} from 'primeng/primeng';
+import{DataTableModule, ButtonModule, InputTextModule,
+ InputMaskModule, CalendarModule} from 'primeng/primeng';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -19,7 +21,12 @@ import { LivroService } from './livro.service';
   imports: [
     BrowserModule,
     HttpClientModule,
-    DataTableModule
+    DataTableModule,
+    ButtonModule,
+    InputTextModule,
+    InputMaskModule,
+    CalendarModule,
+    BrowserAnimationsModule
   ],
   providers: [
     LivroService
